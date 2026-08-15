5b0b47ca0f17450b8794f8e5ec2c0a107432d9ab
Deepak Kumar D <chrisdeepak1006@gmail.com>
2026-08-15 16:42:18 +0530

    Test on ECR module

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -12,7 +12,7 @@ resource "aws_ecr_repository" "main" {
   for_each = toset(var.repositories)
 
   name                 = each.value
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   force_delete         = false
 
   image_scanning_configuration {
